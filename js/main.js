9c78691b37b3a2a194118ad38ae1b120be003b5d
KyleR44 <kylerego447@gmail.com>
2018-10-19 20:58:46 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
+<<<<<<< HEAD
+class Calculator{
+  add(x,y){
+    var n = x + y;
+    return n;
+  }
+  sub(x,y){
+    var n = x - y;
+    return n;
+  }
+  mul(x,y){
+    var n = x * y;
+    return n;
+  }
+  div(x,y){
+    var n = x / y;
+    return n;
+  }
+}
+
+function calculate(operation) {
+  var num1 = parseInt(document.getElementById('val1').value)
+  var num2 = parseInt(document.getElementById('val2').value)
+  var answer = operation(num1,num2)
+  document.getElementById('answer').innerHTML = answer;
+}
+
+
+var calc = new Calculator();
+
+document.getElementById('add').onclick = function(){
+  calculate(calc.add);
+}
+
+document.getElementById('sub').onclick = function(){
+  calculate(calc.sub);
+}
+
+document.getElementById('mul').onclick = function(){
+  calculate(calc.mul);
+}
+
+document.getElementById('div').onclick = function(){
+  calculate(calc.div);
+}
+=======
 // Object oriented Calculator
 class Calculator {
 
@@ -64,3 +110,4 @@ function diviVal(){
   // Displays quotient in DOM
   document.getElementsByTagName('h2')[0].innerHTML = quotient
 }
+>>>>>>> 457855fe4e6fc6d03580ff2946d0f5e00c8ec09d
